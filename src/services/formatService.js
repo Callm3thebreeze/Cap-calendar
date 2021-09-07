ca651f0fa41084c52f0a20e82188d1a9fc5fe0fb
sergiolarrion <sergiolarrion.capgemini@gmail.com>
2021-09-07 11:17:46 +0200

    feature formatService

---
+++ src/services/formatService.js
@@ -0,0 +1,28 @@
+//importar DateService
+export class formatService {
+
+    static getClockFormat(date) {
+        var options = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+        return new Intl.DateTimeFormat('es-ES', options).format(date);
+    }
+
+    static getSystemDate(date) {
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        return new Intl.DateTimeFormat('es-ES', options).format(date);
+    }
+
+    static getMonthDate(date) {
+        var options = { year: 'numeric', month: 'long' };
+        return new Intl.DateTimeFormat('es-ES', options).format(date);
+    }
+
+    static getSubDate(date) {
+        let today = new Date();
+        if (today.getDate() === date.getDate() & today.getMonth() === date.getMonth() & today.getFullYear() === date.getFullYear()) {
+            return "Today";
+        } else {
+            var options = { day: 'numeric', weekday: 'long' };
+            return new Intl.DateTimeFormat('es-ES', options).format(date);
+        }
+    }
+}
