641513f8ffd32bcc09543724b7ed87f3104ee649
seryocardu <seryocardu@gmail.com>
2021-09-14 19:25:14 +0200

    feature subdate

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ import './components/clock-component/clock.js';
 import './components/systemdate-component/systemdate.js';
 import './components/daysofweek/daysofweek.js';
 import './components/monthdate-component/monthdate.js';
+import './components/subdate-component/subdate.js';
