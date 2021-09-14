267fdda3e06559022160571cd41dcfec735581fb
Callm3thebreeze <jesusvillarjimenez@gmail.com>
2021-09-14 18:54:48 +0200

    buttons implemented

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./services/timerservice.js"
 import "./components/clock-component/clock.js";
 import './components/systemdate-component/systemdate.js';
+import "./components/buttons-component/button.js";
 import './components/daysofweek/daysofweek.js';
