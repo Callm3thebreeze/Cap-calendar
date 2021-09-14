cc17b7792664a9a52d0950e083e7cbd367bf6bf7
Rafael <rafaelcpg.rt@gmail.com>
2021-09-14 20:15:34 +0200

    merge main

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import './components/clock-component/clock.js';
-import './components/systemdate-component/systemdate.js';
-import './components/daysofweek/daysofweek.js';
-import './components/gridcalendar/gridcalendar.js';
-=======
 import './services/timerservice.js';
 import './components/clock-component/clock.js';
 import './components/systemdate-component/systemdate.js';
 import './components/daysofweek/daysofweek.js';
 import './components/monthdate-component/monthdate.js';
 import './components/subdate-component/subdate.js';
->>>>>>> main
