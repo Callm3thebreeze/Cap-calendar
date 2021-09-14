0fa7a2623530d0431e0f1766f8647f81c2b3f069
Rafael <rafaelcpg.rt@gmail.com>
2021-09-14 20:27:43 +0200

    merge error with gridcalendar and buttons

--- src/main.js
+++ src/main.js
@@ -4,8 +4,5 @@ import './components/systemdate-component/systemdate.js';
 import './components/daysofweek/daysofweek.js';
 import './components/gridcalendar/gridcalendar.js';
 import './components/monthdate-component/monthdate.js';
-<<<<<<< HEAD
 import "./components/buttons-component/button.js";
-=======
->>>>>>> feature_gridCalendar
 import './components/subdate-component/subdate.js';
