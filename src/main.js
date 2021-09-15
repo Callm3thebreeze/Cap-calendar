7580913c84ce9939213bb19fc5a53914a3e7d0e3
Rafael <rafaelcpg.rt@gmail.com>
2021-09-15 23:23:55 +0200

    folder name changed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './services/timerservice.js';
 import './components/clock-component/clock.js';
 import './components/systemdate-component/systemdate.js';
-import './components/daysofweek/daysofweek.js';
-import './components/gridcalendar/gridcalendar.js';
+import './components/daysofweek-component/daysofweek.js';
+import './components/gridcalendar-component/gridcalendar.js';
 import './components/monthdate-component/monthdate.js';
 import "./components/buttons-component/button.js";
 import './components/subdate-component/subdate.js';
